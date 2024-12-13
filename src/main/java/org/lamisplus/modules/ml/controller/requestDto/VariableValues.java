c7334c765563890e272d4babc795708be7a718fa
AJ-DataFI <victor.ajor@thepalladiumgroup.com>
2024-12-13 08:58:54 +0100

    [ML] Updated the hts pmml v5

--- src/main/java/org/lamisplus/modules/ml/controller/requestDto/VariableValues.java
+++ src/main/java/org/lamisplus/modules/ml/controller/requestDto/VariableValues.java
@@ -40,26 +40,26 @@ public class VariableValues {
     int referred_from_Private_Commercial_Health_facility;
     @JsonProperty("referred_from_Self")
     int referred_from_Self;
-    @JsonProperty("testing_setting_CPMTCT")
-    int testing_setting_CPMTCT;
-    @JsonProperty("testing_setting_CT")
-    int testing_setting_CT;
-    @JsonProperty("testing_setting_OPD")
-    int testing_setting_OPD;
-    @JsonProperty("testing_setting_OTHERS")
-    int testing_setting_OTHERS;
-    @JsonProperty("testing_setting_OUTREACH")
-    int testing_setting_OUTREACH;
-    @JsonProperty("testing_setting_SNT")
-    int testing_setting_SNT;
-    @JsonProperty("testing_setting_STANDALONE_HTS")
-    int testing_setting_STANDALONE_HTS;
-    @JsonProperty("testing_setting_STI")
-    int testing_setting_STI;
-    @JsonProperty("testing_setting_TB")
-    int testing_setting_TB;
-    @JsonProperty("testing_setting_WARD")
-    int testing_setting_WARD;
+    @JsonProperty("testing_settingANC")
+    int testing_settingANC;
+    @JsonProperty("testing_settingCPMTCT")
+    int testing_settingCPMTCT;
+    @JsonProperty("testing_settingCT")
+    int testing_settingCT;
+    @JsonProperty("testing_settingIndex")
+    int testing_settingIndex;
+    @JsonProperty("testing_settingOVC")
+    int testing_settingOVC;
+    @JsonProperty("testing_settingOthers")
+    int testing_settingOthers;
+    @JsonProperty("testing_settingOutreach")
+    int testing_settingOutreach;
+    @JsonProperty("testing_settingTB")
+    int testing_settingTB;
+//    @JsonProperty("testing_setting_TB")
+//    int testing_setting_TB;
+    @JsonProperty("testing_settingWard_Inpatient")
+    int testing_settingWard_Inpatient;
     @JsonProperty("age")
     int age;
     @JsonProperty("first_time_visit")
